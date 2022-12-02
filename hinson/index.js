9048bec5299c5ef91efd55ab145363cb463c5602
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-12-02 13:58:41 -0500

    a

---
+++ hinson/index.js
@@ -0,0 +1,32 @@
+const apple = [
+  {
+    model: "MacBook Air",
+    type: "Laptop",
+    price: "999",
+    CPU: "8-core",
+    GPU: "7-core",
+  },
+  {
+    model: "MacBook Pro",
+    type: "Laptop",
+    price: "1200",
+    CPU: "8-core",
+    GPU: "10-core",
+  },
+  {
+    model: "Iphone 14 Pro",
+    type: "Phone",
+    price: "999",
+    CPU: "none",
+    GPU: "none",
+    screen: "6.1",
+  },
+  {
+    model: "Iphone 14 Pro Max",
+    type: "Phone",
+    price: "999",
+    CPU: "none",
+    GPU: "none",
+    screen: "6.1",
+  },
+];
