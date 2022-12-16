604731ce8a8f799f0f263286748c35c16c7fd53d
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-12-16 13:59:36 -0500

    a

--- hinson/main.js
+++ hinson/main.js
@@ -9,10 +9,13 @@ const DOMSelectors = {
 };
 
 const filphone = apple.filter((element) => element.type == "Phone");
+filphone.forEach((filphone) => console.log(filphone.name));
 const filheadphones = apple.filter((element) => element.type == "Headphones");
+filheadphones.forEach((filheadphone) => console.log(filheadphone.name));
 const fillaptop = apple.filter((element) => element.type == "Laptop");
+fillaptop.forEach((fillaptop) => console.log(fillaptop.name));
 const filipad = apple.filter((element) => element.type == "Ipad");
-
+filipad.forEach((filipad) => console.log(filipad.name));
 function insert() {
   apple.forEach((element) =>
     DOMSelectors.div.insertAdjacentHTML(
@@ -22,4 +25,24 @@ function insert() {
   );
 }
 
+function buttoninsert() {
+  DOMSelectors.div.insertAdjacentHTML(
+    "beforeend",
+    `<p>${filphone.name}</p> <img src="${filphone.image}">`
+  );
+  DOMSelectors.div.insertAdjacentHTML(
+    "beforeend",
+    `<p>${filheadphone.name}</p> <img src="${filheadphone.image}">`
+  );
+  DOMSelectors.div.insertAdjacentHTML(
+    "beforeend",
+    `<p>${fillaptop.name}</p> <img src="${fillaptop.image}">`
+  );
+  DOMSelectors.div.insertAdjacentHTML(
+    "beforeend",
+    `<p>${filipad.name}</p> <img src="${filipad.image}">`
+  );
+}
+
 insert();
+buttoninsert();
