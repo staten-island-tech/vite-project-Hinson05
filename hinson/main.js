e128ee7a78aaa48aebfb15dd87fcc572f561c614
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-01-10 14:02:10 -0500

    a

--- hinson/main.js
+++ hinson/main.js
@@ -7,18 +7,24 @@ const DOMSelectors = {
   headphones: document.getElementById("headphones"),
   div: document.getElementById("app"),
 };
-const toggle = document.getElementById('dark');
-const body = document.querySelector('site');
-toggle.addEventListener('click' , function(){
-  site.style.background = 'black';
-  site.style.color = 'white';
+const toggle = document.getElementById("dark");
+const body = document.querySelector("site");
+toggle.addEventListener("click", function () {
+  site.style.background = "black";
+  site.style.color = "white";
+});
+
+const toggle2 = document.getElementById("light");
+toggle2.addEventListener("click", function () {
+  site.style.background = "white";
+  site.style.color = "black";
 });
 
 function insert() {
   apple.forEach((element) =>
     DOMSelectors.div.insertAdjacentHTML(
       "beforeend",
-      `<div class="flex"><p id="box">${element.model} $${element.price}</p> <img id="imag" src="${element.image}"></div>`
+      `<div class="flex"><p id="box">${element.model} ${element.price}</p> <img id="imag" src="${element.image}"></div>`
     )
   );
 }
@@ -41,37 +47,41 @@ function buttoninsert() {
     `<p>${filipad.model}</p> <img src="${filipad.image}">`
   );
 }
-DOMSelectors.phone.addEventListener("click", function(){
+
+DOMSelectors.phone.addEventListener("click", function () {
   const filphone = apple.filter((element) => element.type == "Phone");
-filphone.forEach((filphone) => console.log(filphone.name));
-  DOMSelectors.div.insertAdjacentHTML(
-    "beforeend",
-    `<p>${filphone.model}</p> <img src="${filphone.image}">`
+  console.log(filphone);
+  DOMSelectors.div.innerHTML = "";
+  filphone.forEach((element) =>
+    DOMSelectors.div.insertAdjacentHTML(
+      "afterend",
+      `<div class="flex><p>${element.model}</p> <img src="${element.image}</div>">`
+    )
   );
 });
-DOMSelectors.ipad.addEventListener("click", function(){
+
+DOMSelectors.ipad.addEventListener("click", function () {
   const filipad = apple.filter((element) => element.type == "Ipad");
-filipad.forEach((filipad) => console.log(filipad.name));
+  filipad.forEach((filipad) => console.log(filipad.name));
   DOMSelectors.div.insertAdjacentHTML(
-    "beforeend",
+    "afterend",
     `<p>${filipad.model}</p> <img src="${filphone.image}">`
   );
 });
-DOMSelectors.headphones.addEventListener("click", function(){
+DOMSelectors.headphones.addEventListener("click", function () {
   const filheadphones = apple.filter((element) => element.type == "Headphones");
-filheadphones.forEach((filheadphone) => console.log(filheadphone.name));
+  filheadphones.forEach((filheadphone) => console.log(filheadphone.name));
   DOMSelectors.div.insertAdjacentHTML(
-    "beforeend",
+    "afterend",
     `<p>${filheadphones.model}</p> <img src="${filphone.image}">`
   );
 });
-DOMSelectors.laptop.addEventListener("click", function(){
+DOMSelectors.laptop.addEventListener("click", function () {
   const fillaptop = apple.filter((element) => element.type == "Laptop");
-fillaptop.forEach((fillaptop) => console.log(fillaptop.name));
+  fillaptop.forEach((fillaptop) => console.log(fillaptop.name));
   DOMSelectors.div.insertAdjacentHTML(
-    "beforeend",
+    "afterend",
     `<p>${fillaptop.model}</p> <img src="${filphone.image}">`
   );
 });
 insert();
-buttoninsert();
